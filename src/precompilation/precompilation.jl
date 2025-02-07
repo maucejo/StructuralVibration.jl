2b14df0ccd22d8c54f2c098152aae3005759ef9e
Mathieu Aucejo <mathieu.aucejo@gmail.com>
2025-02-07 13:25:17 +0100

    precompile.jl

--- src/precompilation/precompilation.jl
+++ src/precompilation/precompilation.jl
@@ -1,5 +1,16 @@
 @setup_workload begin
 
+    # sdof + sdof solvers
+    @compile_workload begin
+
+    end
+
+    # noise
+    @compile_workload begin
+
+    end
+
+    # Modal solvers
     @compile_workload begin
 
     end
